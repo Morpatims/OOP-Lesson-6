64c0410c928386480a72e06f67cebd3ab77c97f4
morpatims <kverteletski@mail.ru>
2023-11-21 14:53:02 +0300

    Reworked the code
    Reworked the code to comply with the SRP principle

--- src/main/java/ru/geekbrains/lesson6/srp2/Program.java
+++ src/main/java/ru/geekbrains/lesson6/srp2/Program.java
@@ -2,17 +2,12 @@ package ru.geekbrains.lesson6.srp2;
 
 public class Program {
 
-    /**
-     * TODO: Переработать структуру приложения Order, приложение должно соответствовать
-     *  принципу SRP.
-     * @param args
-     */
     public static void main(String[] args) {
 
-        Order order = new Order();
-        order.inputFromConsole();
-        order.saveToJson();
+        OrderInput orderInput = new OrderInput();
+        Order order = orderInput.inputFromConsole();
 
+        OrderJsonSaver orderJsonSaver = new OrderJsonSaver();
+        orderJsonSaver.saveToJson(order);
     }
-
 }
